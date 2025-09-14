1f8304fd2a13d1381e8305ca344258f10fabf9a0
mumbosteve <stevemumbo3@gmail.com>
2025-09-14 10:12:18 +0300

    initial commit

---
+++ alx-react-app-new/src/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', fontSize: '18px', textAlign: 'center', backgroundColor: '#f0f8ff' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;

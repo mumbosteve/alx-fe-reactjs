5e389a34045d73afe8f2cf31babeef2ee13098df
mumbosteve <stevemumbo3@gmail.com>
2025-09-14 19:48:55 +0300

    Fifth commit

---
+++ alx-react-app-props/src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+function Counter() {
+  // initialize state
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h2>Counter App</h2>
+      <p style={{ fontSize: '20px' }}>Current Count: {count}</p>
+
+      {/* Buttons */}
+      <button onClick={() => setCount(count + 1)} style={{ margin: '5px' }}>
+        Increment
+      </button>
+      <button onClick={() => setCount(count - 1)} style={{ margin: '5px' }}>
+        Decrement
+      </button>
+      <button onClick={() => setCount(0)} style={{ margin: '5px' }}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default Counter;

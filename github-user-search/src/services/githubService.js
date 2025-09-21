b88663ce170d22ff89b7cd02cf5d631c64871945
mumbosteve <stevemumbo3@gmail.com>
2025-09-21 14:41:48 +0300

    third commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,14 @@
-// src/services/githubService.js
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
-    return response.data;
-  } catch (error) {
-    console.error("GitHub API error:", error);
-    throw error;
-  }
+const BASE_URL = "https://api.github.com";
+
+export const fetchAdvancedUserData = async (username, location, minRepos) => {
+  let query = "";
+
+  if (username) query += `${username} in:login `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>=${minRepos}`;
+
+  const response = await axios.get(`${BASE_URL}/search/users?q=${query}`);
+  return response.data;
 };

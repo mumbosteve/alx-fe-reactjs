25a3a8783377ecc9b950799d43540f107371f0a4
mumbosteve <stevemumbo3@gmail.com>
2025-09-21 15:02:24 +0300

    Error fixing

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,14 +1,28 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com";
+const BASE_URL = "https://api.github.com/search/users";
 
-export const fetchAdvancedUserData = async (username, location, minRepos) => {
-  let query = "";
+/**
+ * Fetch GitHub users based on search criteria
+ * @param {string} username - GitHub username (or partial)
+ * @param {string} location - Location filter
+ * @param {number} minRepos - Minimum number of public repositories
+ */
+export const fetchUserData = async (username, location, minRepos) => {
+  try {
+    let query = "";
 
-  if (username) query += `${username} in:login `;
-  if (location) query += `location:${location} `;
-  if (minRepos) query += `repos:>=${minRepos}`;
+    if (username) query += `${username}+`;
+    if (location) query += `location:${location}+`;
+    if (minRepos) query += `repos:>=${minRepos}`;
 
-  const response = await axios.get(`${BASE_URL}/search/users?q=${query}`);
-  return response.data;
+    // Clean trailing '+'
+    query = query.trim().replace(/\+$/, "");
+
+    const response = await axios.get(`${BASE_URL}?q=${query}`);
+    return response.data; // contains items array with users
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
 };

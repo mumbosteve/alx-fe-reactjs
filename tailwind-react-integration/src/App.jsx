ebcab9e00974a0880a2cbb63bd0b9e7d8f1be12f
mumbosteve <stevemumbo3@gmail.com>
2025-09-26 08:43:34 +0300

    initial commit

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,8 +1,9 @@
+import UserProfile from "./components/UserProfile"
+
 function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-blue-600 text-white">
-      <h1 className="text-5xl font-bold mb-4">Tailwind is working!</h1>
-      <p className="text-lg">Styled with utility classes in React + Vite</p>
+    <div>
+      <UserProfile />
     </div>
   )
 }

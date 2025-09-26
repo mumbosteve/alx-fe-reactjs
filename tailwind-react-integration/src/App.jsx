9349224a9a074eb111d171db209c4694ab8b97f2
mumbosteve <stevemumbo3@gmail.com>
2025-09-26 08:39:15 +0300

    initial commit

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-blue-600 text-white">
+      <h1 className="text-5xl font-bold mb-4">Tailwind is working!</h1>
+      <p className="text-lg">Styled with utility classes in React + Vite</p>
+    </div>
+  )
+}
+
+export default App

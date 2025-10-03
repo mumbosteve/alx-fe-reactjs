d636b4cabcdbd6b11fccf7b1d8f65ff1d7601273
mumbosteve <stevemumbo3@gmail.com>
2025-10-03 16:16:27 +0300

    initial commit

--- recipe-shairing-platform/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-
-    <h1 className="text-4xl text-red-500">Hello Tailwind v4 🎉</h1>
-
-  );
-}
-
-export default App;
--- tailwind-react-intergration/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">🚀 Tailwind is working!</h1>
-    </div>
-  )
-}
-
-export default App

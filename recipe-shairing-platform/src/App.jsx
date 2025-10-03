552024238b25a9f6fdba358a3f08a3c071f2f0e5
mumbosteve <stevemumbo3@gmail.com>
2025-10-03 12:20:29 +0300

    initial commit

--- recipe-shairing-platform/src/App.jsx
+++ recipe-shairing-platform/src/App.jsx
@@ -1,10 +1,9 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-500">
-      Hello Tailwind!
-    </h1>
+
+    <h1 className="text-4xl text-red-500">Hello Tailwind v4 🎉</h1>
+
   );
 }
-
 
 export default App;

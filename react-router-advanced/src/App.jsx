ffc71c26ec0b2221a23f36b07e7f1760838ec0db
mumbosteve <stevemumbo3@gmail.com>
2025-10-11 21:46:04 +0300

    Route

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
@@ -7,37 +12,37 @@ import BlogPost from "./pages/BlogPost";
 import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-function App() {
+export default function App() {
   return (
-    <div className="p-6">
-      <nav className="flex gap-4 mb-6">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/blog/1">Blog Post 1</Link>
-        <Link to="/login">Login</Link>
-      </nav>
+    <BrowserRouter>
+      <div className="p-6">
+        <nav className="flex gap-4 mb-6">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/blog/1">Blog Post 1</Link>
+          <Link to="/login">Login</Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
 
-        {/* Protected Profile route */}
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
+          {/* Protected Route */}
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Dynamic route for blog posts */}
-        <Route path="/blog/:id" element={<BlogPost />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
+          {/* Dynamic Blog Route */}
+          <Route path="/blog/:id" element={<BlogPost />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
-
-export default App;

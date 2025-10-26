377b5f223a21677e2d8ccba359c668847d4faf1f
mumbosteve <stevemumbo3@gmail.com>
2025-10-26 10:34:37 +0300

    First commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,14 @@
-import ProfilePage from './ProfilePage';
-import UserContext from './components/UserContext';
+import UserProfile from './components/UserProfile';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
   return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
+    <div>
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+    </div>
   );
 }
 

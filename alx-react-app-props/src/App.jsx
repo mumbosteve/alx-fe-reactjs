37d307dd3e5d631103488fff59b97e882ad4414d
mumbosteve <stevemumbo3@gmail.com>
2025-10-26 10:47:00 +0300

    First commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,6 +1,7 @@
 import ProfilePage from './ProfilePage';
 import UserContext from './components/UserContext';
 
+
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,13 @@
-function UserProfile(props) {
+import React, { useContext } from "react";
+import { UserContext } from "../UserContext";
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>{userData.name}</h2>
+      <p>Email: {userData.email}</p>
     </div>
   );
 }

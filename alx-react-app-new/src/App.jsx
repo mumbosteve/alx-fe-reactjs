dc47e07d83a0294c66929b0b7fa83d79464e94ca
mumbosteve <stevemumbo3@gmail.com>
2025-10-26 10:07:00 +0300

    First commit

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,11 +1,14 @@
-import Counter from './components/Counter';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
   return (
-    <div>
-      <h1>Welcome to My React App</h1>
-      <Counter />
-    </div>
+    // Provide the userData to the entire component tree
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
